b3b714e69abe9bfc4aca953440bb5e29053921cc
mxnojkumar <manojkv5696@gmail.com>
2024-09-13 23:31:52 +0530

    Secure passwords with env_variables

--- company_webapp/send_mails.py
+++ company_webapp/send_mails.py
@@ -1,11 +1,12 @@
 import smtplib, ssl
+import os
 
 def send_mail(full_message):
     host = "smtp.gmail.com"
     port = 465
     
     username = "copycatpresentsir@gmail.com"
-    password = "ldzdfovjddllfjeb"
+    password = os.getenv("PASSWORD")
 
     receiver = "copycatpresentsir@gmail.com"
     context = ssl.create_default_context()
